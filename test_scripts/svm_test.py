112b049bd20b64f8b8d942684d6cdd3a83c19e48
Chukwuemeka Eze <73246753+ezecpaul@users.noreply.github.com>
2023-03-25 20:28:26 +1100

    Update svm_test.py

--- test_scripts/svm_test.py
+++ test_scripts/svm_test.py
@@ -7,3 +7,7 @@ from sklearn import svm
 from sklearn.metrics import make_scorer
 from sklearn.metrics import mean_squared_error
 from models.svm import SVM
+ROOT_DIR ="
+model_dir = os.path.join(ROOT_DIR, 'model_pkl')
+svr_model = joblib.load(os.path.join(model_dir,"svr_model.pkl")
+                        
