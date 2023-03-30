53915b4c36a46d7d2bd3fa3eb3eea5b9a0e3b9c6
ezecpaul <eze.cpaul@gmail.com>
2023-03-30 13:07:19 +1100

    update #4: models.svm.py and utils.py updated

--- models/svm.py
+++ models/svm.py
@@ -1,45 +1,91 @@
 import os
 import sys
-import joblib
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
 from sklearn import svm
-from sklearn.metrics import make_scorer
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, make_scorer
+import joblib
+sys.path.append('.')
+ROOT_DIR = os.path.dirname(os.path.abspath('COMP_9417_23_Project')) # project Directory
+saved_model_dir = os.path.join(ROOT_DIR, 'model_pkl')
+model_file = os.path.join(saved_model_dir, 'svr_model.pkl')
+svr_metrics_dir =os.path.join(model_file, 'svr_metrics')
 
 
-class SVM:
+class SVR_Model:
     @staticmethod
     # define base model
-    def svm_tuning(X:pd.Dataframe, y:pd.DataFrame) -> pd.DataFrame:
+    def svm_tuning(X:pd.DataFrame, y:pd.DataFrame):
         
-        # gridsearch parameter declaration
+        # Load and return saved model if exist
+        if os.path.exists(os.path.join(svr_metrics_dir, 'svr_params.csv')):
+            try:
+                model_best = pd.read_csv(os.path.join(svr_metrics_dir, 'svr_params.csv'))
+                print(f'\nHere is best param for svm.SVR model \n {model_best}')                
+                return
+            except:
+                print(f'\nError loading existing best parameter file')
+                sys.exit(0)       
+        
+        # else: Do a grid-search and build new model
+        print('\nSVR Tuning in progress... ')
+        X = np.array(X)
+        y = np.array(y)
+        y = y[:,0] # build model will only one response variable (1st Response Variable)
+        
+        # grid-search parameters range
         param = {   'kernel': ('linear', 'rbf', 'poly'),
-                    'C':[10, 100, 1000],
+                    'C':[1000.0, 10000.0, 15000.0],
                     'degree': [2, 3],
-                    'coef0': [0.01, 0.5, 1],
-                    'gamma': ['auto', 0.1],
+                    'coef0': [0.01, 0.05, 0.1],
+                    'gamma': 'auto',
         }
 
-        # model declaration           
-        svr = svm.SVR() 
-
         # scorer
-        scorer = make_scorer(mean_squared_error, greater_is_better=False)
-        
-        # hyper-parameter tuning
-        grid_search = GridSearchCV( estimator = svr,
+        scorer = make_scorer(mean_squared_error, greater_is_better=False)       
+
+        # define gridserach
+        grid_search = GridSearchCV( estimator = svm.SVR(), # base model declaration
                                     param_grid = param, 
                                     cv = 3, 
                                     n_jobs = -1, 
                                     verbose = 2,
                                     scoring=scorer
-                                    )
+        )
 
-        # Fit optimised base_models
-        svr_model = grid_search.fit(X, y)
+        # tune model with grid_search
+        svr_search_run = grid_search.fit(X, y)
 
-        # Save model as Pickle
-        joblib.dump(svr_model, "svr_model.pkl")
-        return svr_model
-        
+        # save csv of grid_search results
+        pd.DataFrame(svr_search_run.cv_results_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_cv_result.csv'), index=False)
+        pd.DataFrame(svr_search_run.best_estimator_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_estimator.csv'), index=False)
+        pd.DataFrame(svr_search_run.best_params_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_params.csv'), index=False)
+        pd.DataFrame(svr_search_run.best_score_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_score.csv'), index=False)
+
+        print(f'\n Hyperparameter tuning completed with metrics saved in {svr_metrics_dir} \n  \
+                Best parameters:\n {svr_search_run.best_params_}')
+        return 
+
+from utils import util
+
+# Runing snipet #1
+# process data from train dataset 'training.csv'
+X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test = util.process_data('training.csv')
+
+# best params = {'C': 10000.0, 'coef0':0.01, 'degree': 3, 'gamma': 'auto', 'kernel': 'poly'}
+model = svm.SVR(C= 10000.0, 
+                coef0=0.01, 
+                degree= 3, 
+                gamma= 'auto', 
+                kernel= 'poly')
+
+# predict using best model
+pred = util.fit_predict(model, 
+                      X_train, 
+                      y_train, 
+                      X_test)
+
+# Compute RMSE for each response variable and column-wise Average-RMSE
+RMSEs, Ave_RMSE = util.average_RMSE(y_test, pred)
+print(f'\nColumns [Ca, P, pH, SOC , Sand] RMSEs: \n {RMSEs} \n Average RMSE: {Ave_RMSE}')
--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 import os
-import numpy as np
+import sys
 import pandas as pd
-from sklearn.metrics import mean_squared_error
+import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
 import joblib
 sys.path.append('.')
 ROOT_DIR = os.path.dirname(os.path.abspath('COMP_9417_23_Project')) # project Directory
+data_dir = os.path.join(ROOT_DIR, 'data')
+processed_data_dir = os.path.join(ROOT_DIR, 'data_pkl')
+saved_model_dir = os.path.join(ROOT_DIR, 'model_pkl')
 
 # Define some utility functions in util class
 class util:
@@ -15,12 +20,16 @@ class util:
     ''' 
     # Compute Column-wise Average RMSE 
     @staticmethod
-    def average_RMSE(y_true: np.ndarray, y_pred: np.ndarray) -> list:
-        column_len = y.shape[1] 
+    def average_RMSE(y_true: pd.DataFrame, y_pred):
+        columns = y_pred.columns.values
+        y_true = np.array(y_true)
+        y_pred = np.array(y_pred)
+
+        column_len = y_true.shape[1] 
         errors = np.zeros(column_len)
         for i in range(column_len):
-            errors[i] = mean_squared_error(y[:,i], y_hat[:,i], squared=False)
-        return [errors, np.average(errors)]
+            errors[i] = mean_squared_error(y_true[:,i], y_pred[:,i], squared=False)
+        return errors, np.average(errors)
     
     # Feature Variables Standardization
     @staticmethod
@@ -32,28 +41,97 @@ class util:
         scaler = StandardScaler()
         scaler.fit(X_train)
         scaled_Xtrain = pd.DataFrame(data=scaler.transform(X_train), columns=columns)
-        scaled_Xtest= pd.DataFrame(data=scaler.transform(X_test), columns=columns)
+        scaled_Xtest= pd.DataFrame(data=scaler.transform(X_test), columns=columns)        
         return scaled_Xtrain, scaled_Xtest
     
     # Prepare and Save Submission file
     @staticmethod
-    def submission(output_sample_file: pd.DataFrame, pred: np.ndarray) -> pd.DataFrame:
+    def submission(output_sample_file: pd.DataFrame, pred) -> pd.DataFrame:
+        pred = np.array(pred)
         output = pd.read_csv(output_sample_file)
+
         output['Ca'] = pred[:,0]
         output['P'] = pred[:,1]
         output['pH'] = pred[:,2]
         output['SOC'] = pred[:,3]
         output['Sand'] = pred[:,4]
-        output.to_csv('predictions.csv', index = False)
-        print('prediction file saved')
+        output.to_csv(os.path.join(processed_data_dir, 'predictions.csv'), index = False)
+        print(f'prediction file saved to {processed_data_dir}')
 
     # Perform prediction using a built model
     @staticmethod
-    def model_predict(model: joblib, X_train: pd.DataFrame, y_train: pd.DataFrame):
-        n_rows = y_train.shape[0]
+    def fit_predict(model, 
+                      X_train: pd.DataFrame, 
+                      y_train: pd.DataFrame, 
+                      X_test: pd.DataFrame):
+        columns = y_train.columns.values
+        X_train = np.array(X_train)
+        y_train = np.array(y_train)
+        X_test = np.array(X_test)
+
         n_columns = y_train.shape[1]
-        y_preds = np.zeros((n_rows, n_columns))
-        for i in range(n_columns):
-            model.fit(X_train, y_train[:,i])
-            y_preds[:,i] = model.predict(X_test).astype(float)
-        return y_preds
+        n_rows = X_test.shape[0]
+        y_pred = np.zeros((n_rows, n_columns))        
+        for i in range(n_columns):            
+            y_pred[:,i]= model.fit(X_train, y_train[:,i]).predict(X_test).astype(float)
+        y_pred =pd.DataFrame(data=y_pred, columns=columns)
+        return y_pred
+
+    # Process Dataset into Train and Test sets   
+    @staticmethod
+    def process_data(   training_data_file: str, 
+                        test_data_file: str = None ):
+        # check if files exist, load and return them
+        if os.path.exists(os.path.join(processed_data_dir, 'X_train.csv')):
+            try:
+                X_train = pd.read_csv(os.path.join(processed_data_dir, 'X_train.csv'))
+                X_test = pd.read_csv(os.path.join(processed_data_dir, 'X_test.csv'))
+                y_train = pd.read_csv(os.path.join(processed_data_dir, 'y_train.csv'))
+                y_test = pd.read_csv(os.path.join(processed_data_dir, 'y_test.csv'))
+                X_train_scaled = pd.read_csv(os.path.join(processed_data_dir,'X_train_scaled.csv'))
+                X_test_scaled = pd.read_csv(os.path.join(processed_data_dir,'X_test_scaled.csv'))
+
+                print('\nSix(6) Dataframes (X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test), returned.')
+                return X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test
+            except:
+                print("Error reading one of the files")            
+
+        print('Data pre-processing in progress...\n')
+        # # process test dataset if required
+        # if test_data_file:
+        #     test_file = os.path.join(data_dir, test_data_file)
+        #     test = pd.read_csv(test_file)
+        #     test.drop(['PIDN', 'Depth'], axis=1, inplace=True)
+
+        # Read train datasets
+        train_file = os.path.join(data_dir, training_data_file)
+        train = pd.read_csv(train_file)
+        train.dropna(inplace=True) # drop nan row
+        labels = train[['Ca','P','pH','SOC','Sand']] # y/labels
+
+        # drop response values(labels), ID, and categorical variable from predictors
+        train.drop(['Ca', 'P', 'pH', 'SOC', 'Sand', 'PIDN','Depth'], axis=1, inplace=True)        
+
+        # split into train/test sets
+        X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.20, random_state=100) 
+        
+        # reset indexes
+        X_train.reset_index(drop=True, inplace=True) 
+        X_test.reset_index(drop=True, inplace=True)
+        y_train.reset_index(drop=True, inplace=True)
+        y_test.reset_index(drop=True, inplace=True)
+
+        # save csv files
+        X_train.to_csv(os.path.join(processed_data_dir,'X_train.csv'), index=False)
+        X_test.to_csv(os.path.join(processed_data_dir,'X_test.csv'), index=False)
+        y_train.to_csv(os.path.join(processed_data_dir,'y_train.csv'), index=False)
+        y_test.to_csv(os.path.join(processed_data_dir,'y_test.csv'), index=False)
+        # joblib.dump(X_train, os.path.join(processed_data_dir, 'X_train.pkl')) #save to pickle
+
+        # scale and save tranformed data
+        X_train_scaled, X_test_scaled = util.standardize(X_train, X_test)
+        X_train_scaled.to_csv(os.path.join(processed_data_dir,'X_train_scaled.csv'), index=False)
+        X_test_scaled.to_csv(os.path.join(processed_data_dir,'X_test_scaled.csv'), index=False)
+
+        print('\nSix(6) Dataframes (X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test), returned.')
+        return X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test
