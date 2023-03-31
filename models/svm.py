5a9696e692d40adc70822de84cd43fab439498ea
ezecpaul <eze.cpaul@gmail.com>
2023-04-01 08:37:41 +1100

    update #5: fixed bugs in code

--- models/svm.py
+++ models/svm.py
@@ -9,8 +9,8 @@ import joblib
 sys.path.append('.')
 ROOT_DIR = os.path.dirname(os.path.abspath('COMP_9417_23_Project')) # project Directory
 saved_model_dir = os.path.join(ROOT_DIR, 'model_pkl')
-model_file = os.path.join(saved_model_dir, 'svr_model.pkl')
-svr_metrics_dir =os.path.join(model_file, 'svr_metrics')
+# model_file = os.path.join(saved_model_dir, 'svr_model.pkl')
+svr_metrics_dir = os.path.join(saved_model_dir, 'svr_metrics')
 
 class SVR_Model:
     @staticmethod
@@ -20,8 +20,8 @@ class SVR_Model:
         # Load and return saved model if exist
         if os.path.exists(os.path.join(svr_metrics_dir, 'svr_params.csv')):
             try:
-                model_best = pd.read_csv(os.path.join(svr_metrics_dir, 'svr_params.csv'))
-                print(f'\nHere is best param for svm.SVR model \n {model_best}')                
+                model_best_param = pd.read_csv(os.path.join(svr_metrics_dir, 'svr_params.csv'))
+                print(f'\nHere is best param for svm.SVR model \n {model_best_param}')                
                 return
             except:
                 print(f'\nError loading existing best parameter file')
@@ -31,21 +31,20 @@ class SVR_Model:
         print('\nSVR Tuning in progress... ')
         X = np.array(X)
         y = np.array(y)
-        y = y[:,0] # build model will only one response variable (1st Response Variable)
+        y = y[:,1] # build model will only one response variable (1st Response Variable)
         
         # grid-search parameters range
         param = {   'kernel': ('linear', 'rbf', 'poly'),
                     'C':[1000.0, 10000.0, 15000.0],
                     'degree': [2, 3],
-                    'coef0': [0.01, 0.05, 0.1],
-                    'gamma': 'auto',
+                    'coef0': [0.01, 0.05, 0.1]
         }
 
         # scorer
         scorer = make_scorer(mean_squared_error, greater_is_better=False)       
 
         # define gridserach
-        grid_search = GridSearchCV( estimator = svm.SVR(), # base model declaration
+        grid_search = GridSearchCV( estimator = svm.SVR( gamma= 'auto'), # base model declaration
                                     param_grid = param, 
                                     cv = 3, 
                                     n_jobs = -1, 
@@ -57,10 +56,12 @@ class SVR_Model:
         svr_search_run = grid_search.fit(X, y)
 
         # save csv of grid_search results
-        pd.DataFrame(svr_search_run.cv_results_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_cv_result.csv'), index=False)
-        pd.DataFrame(svr_search_run.best_estimator_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_estimator.csv'), index=False)
-        pd.DataFrame(svr_search_run.best_params_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_params.csv'), index=False)
-        pd.DataFrame(svr_search_run.best_score_).to_csv(os.path.join(saved_model_dir,'svr_metrics','svr_score.csv'), index=False)
+        pd.DataFrame(svr_search_run.cv_results_).to_csv(os.path.join(svr_metrics_dir,'svr_cv_result.csv'), index=False)
+        pd.DataFrame(svr_search_run.best_estimator_).to_csv(os.path.join(svr_metrics_dir,'svr_estimator.csv'), index=False)
+        pd.DataFrame(svr_search_run.best_params_).to_csv(os.path.join(svr_metrics_dir,'svr_params.csv'), index=False)
+        pd.DataFrame(svr_search_run.best_score_).to_csv(os.path.join(svr_metrics_dir,'svr_score.csv'), index=False)
+
+        joblib.dump(svr_search_run.best_params_, os.path.join(processed_data_dir, 'X_train.pkl')) #save to pickle
 
         print(f'\n Hyperparameter tuning completed with metrics saved in {svr_metrics_dir} \n  \
                 Best parameters:\n {svr_search_run.best_params_}')
--- test_scripts/svm_test.py
+++ test_scripts/svm_test.py
@@ -6,7 +6,6 @@ import pandas as pd
 from sklearn import svm
 from sklearn.metrics import make_scorer
 from sklearn.metrics import mean_squared_error
-from models.svm import SVM
 sys.path.append('.')
 ROOT_DIR = os.path.dirname(os.path.abspath('COMP_9417_23_Project')) # project Directory
 saved_model_dir = os.path.join(ROOT_DIR, 'model_pkl')
@@ -15,19 +14,23 @@ model_file = os.path.join(saved_model_dir, 'svr_model.pkl')
 svr_metrics_dir =os.path.join(model_file, 'svr_metrics')                        
 
 from utils import util
-from models import SVR_Model # use this when tuning is not done yet.
+from models.svm import SVR_Model # use this when tuning is not done yet.
 
 # step 1:
+# read data sets (X_train, y_train, X_test, y_test)
+X_train = pd.read_csv(os.path.join(data_files_dir, 'X_train.csv'))
+X_test = pd.read_csv(os.path.join(data_files_dir, 'X_test.csv'))
+y_train = pd.read_csv(os.path.join(data_files_dir, 'y_train.csv'))
+y_test = pd.read_csv(os.path.join(data_files_dir, 'y_test.csv'))
 
-# Read X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test
 
 # step 2:
 # Read best parameter file "model_pkl\svr_metrics\svr_best_metric.csv"
 # example:best params = {'C': 10000.0, 'coef0':0.01, 'degree': 3, 'gamma': 'auto', 'kernel': 'poly'}
 
 #Step 3: Use best parameter to build define and build model
-model = svm.SVR(C= 10000.0, 
-                coef0=0.01, 
+model = svm.SVR(C= 15000.0, 
+                coef0=0.1, 
                 degree= 3, 
                 gamma= 'auto', 
                 kernel= 'poly')
